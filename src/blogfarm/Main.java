72b59eda3fde232f351b69385506baeb34e2c628
GianiWVL <Giani@MacBook-Pro-van-Giani.local>
2014-06-25 18:24:45 +0200

    Implemented feedparser

--- src/blogfarm/Main.java
+++ src/blogfarm/Main.java
@@ -3,5 +3,12 @@ package blogfarm;
 public class Main {
     public static void main(String[] args) {
         MainMenu main = new MainMenu();
+        /*RSSFeedParser parser = new RSSFeedParser("http://www.vogella.com/article.rss");
+        Feed feed = parser.readFeed();
+        System.out.println(feed);
+        for(FeedMessage message : feed.getMessages()) {
+        System.out.println(message);*/
+
+        //}
     }    
 }
